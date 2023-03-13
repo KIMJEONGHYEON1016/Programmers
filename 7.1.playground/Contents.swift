12492889edb4ab729ff24ebe8696c053d34ae55a
KIMJEONGHYEON1016 <127102729+KIMJEONGHYEON1016@users.noreply.github.com>
2023-03-13 22:02:06 +0900

    Create Contents.swift

---
+++ 7.1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ price:Int) -> Int {
+  var result: Double = Double(price)
+    if price >= 100000 {
+        result *= 0.95
+    }else if price >= 300000 {
+        result *= 0.9
+    }else if price >= 500000 {
+        result *= 0.8}
+    return Int(result)
+}
