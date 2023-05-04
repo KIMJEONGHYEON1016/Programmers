9932bb7d58277f0b54ad7eca2953bfd3f629c556
KIMJEONGHYEON1016 <127102729+KIMJEONGHYEON1016@users.noreply.github.com>
2023-05-05 02:38:31 +0900

    8주차- 2

---
+++ 8주차- 2.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/*문제 설명
+
+자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.*/
+
+func solution(_ n:Int) -> Int {
+   var a = n-1
+    for i in 2...a/2 {
+      if a % i == 0 {
+          return i
+      }
+    }
+    return a
+}
